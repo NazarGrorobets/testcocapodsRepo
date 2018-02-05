712c4f9a5644d8db57e0664a1ffc4dfb951c110b
Nazar Gorobets <n.gorobets@spheremall.com>
2018-02-05 22:35:47 +0200

    Initial commit

---
+++ tetsCocoapods/tetsCocoapods.h
@@ -0,0 +1,19 @@
+//
+//  tetsCocoapods.h
+//  tetsCocoapods
+//
+//  Created by Nazar Gorobets on 2/5/18.
+//  Copyright © 2018 SphereMall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tetsCocoapods.
+FOUNDATION_EXPORT double tetsCocoapodsVersionNumber;
+
+//! Project version string for tetsCocoapods.
+FOUNDATION_EXPORT const unsigned char tetsCocoapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tetsCocoapods/PublicHeader.h>
+
+
